9699dfeb1b03556dfebb0bda750dc7a101d3850e
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-07-15 14:50:14 +0200

    Add files via upload

---
+++ Opdracht Reference type Object & Arrays/script.js
@@ -0,0 +1,31 @@
+ //Deel 1 Objecten
+
+let person = {
+    name: 'dylan',
+    age:  53
+}; 
+console.log (person);
+
+
+
+
+// Dot notation
+person.name = 'John',
+
+console.log(person.name);
+
+//Bracket notation
+person ['name'] ='mary';
+
+console.log (person.name);
+
+let evaluations = {
+    value: 7 * 10 * 9 
+    };
+
+    console.log (evaluations);
+
+//Deel 2 Arrays
+
+let selectedColours = ['green', 'blauw', 'rood',5, 'Greetings: Hi Ik ben een object'];   
+console.log(selectedColours[2])
